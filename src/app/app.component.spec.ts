2fab192fbf33f16a4cfed553a6eacd6f0270dd15
eras0r <er4s0r@gmail.com>
2017-06-09 14:39:02 +0200

    add shared module with navbar component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import {async, TestBed} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
       imports: [
         RouterTestingModule
@@ -27,6 +29,13 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
 
     expect(app.title).toEqual('app');
+  }));
+
+  it('should render a router-outlet component', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('gdb-navbar')).toBeDefined();
   }));
 
   it('should render a router-outlet component', async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {SharedModule} from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {PaintColorsModule} from './paint-colors/paint-colors.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import {HttpModule} from '@angular/http';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    PaintColorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
