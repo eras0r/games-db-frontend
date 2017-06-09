5f8057b073d31ed7b8cf682cc4522b25863a3f42
eras0r <er4s0r@gmail.com>
2017-06-09 09:46:25 +0200

    configure karma unit tests to run with PhantomJS

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,18 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// polyfills required to run the tests within PhantomJS
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/string';
+import 'core-js/es6/array';
+
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-import { getTestBed } from '@angular/core/testing';
+import {getTestBed} from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
@@ -17,7 +23,8 @@ declare const __karma__: any;
 declare const require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function () {
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
